d3046f1793065ca21b589fb0a5e621d929c1ab8f
TianYue Ren <godners@163.com>
2026-04-24 17:57:28 +0800

    2026.04.24.20

--- .github/actions/git-release/worker-info.bash
+++ .github/actions/git-release/worker-info.bash
@@ -7,6 +7,3 @@ echo "tag=${TAG}" >> $GITHUB_OUTPUT
 
 TIME=$(date -u -d '8 hours' '+%F %T')
 echo "time=${TIME}" >> $GITHUB_OUTPUT
-
-echo "Generated tag: ${TAG}" >> $GITHUB_STEP_SUMMARY
-echo "Generated time (GMT+8): ${TIME}" >> $GITHUB_STEP_SUMMARY
