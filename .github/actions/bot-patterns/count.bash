135e0205531bdff44bab2c5ae2563e491c789344
TianYue Ren <godners@163.com>
2026-04-27 16:49:08 +0800

    2026.04.27.33

--- .github/actions/bot-patterns/count.bash
+++ .github/actions/bot-patterns/count.bash
@@ -7,8 +7,8 @@ COUNT_PATTERNS=$(echo "${PATTERNS}" | sed '/^$/d' | wc -l)
 echo "COUNT_PATTERNS=${COUNT_PATTERNS}" >> $GITHUB_OUTPUT
 if [ -z "$PATHS" ]
 then
-    echo "COUNT_FILES=0" >> $GITHUB_OUTPUT
+    echo "COUNT_PATHS=0" >> $GITHUB_OUTPUT
 else
-    COUNT_FILES=$(echo "PATHS" | awk -F \'${SEPARATOR}\' '{print NF}')
-    echo "COUNT_FILES=${COUNT_FILES}" >> $GITHUB_OUTPUT
+    COUNT_FILES=$(echo "$PATHS" | awk -F \'${SEPARATOR}\' '{print NF}')
+    echo "COUNT_PATHS=${COUNT_PATHS}" >> $GITHUB_OUTPUT
 fi
