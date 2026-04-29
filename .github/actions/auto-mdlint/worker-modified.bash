061110036d9066a5ef3cb4ea5f28097065a2bd30
TianYue Ren <godners@163.com>
2026-04-29 13:29:32 +0800

    2026.04.29.20

--- .github/actions/auto-mdlint/worker-modified.bash
+++ .github/actions/auto-mdlint/worker-modified.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "[BASH] Auto MDLint (Modified)"
-echo ".github/actions/auto-mdlint/worker-modified.bash
+echo ".github/actions/auto-mdlint/worker-modified.bash"
 
 TOTAL_MODIFIED=$(git status --porcelain | wc -l)
 echo "TOTAL_MODIFIED=${TOTAL_MODIFIED}" >> $GITHUB_ENV
